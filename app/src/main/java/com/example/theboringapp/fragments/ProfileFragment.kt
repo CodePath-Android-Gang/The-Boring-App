c2fadc7148a987fba6d96c0007212c028c4fdfef
Shaun Gold <sfgold2@gmail.com>
2022-03-03 20:29:09 -0800

    added sprint 2 gif

--- app/src/main/java/com/example/theboringapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/theboringapp/fragments/ProfileFragment.kt
@@ -63,6 +63,8 @@ class ProfileFragment : Fragment() {
                                 CurUser.addUnique("friendsList", objId.getString("username"))
                                 CurUser.saveInBackground()
                             }
+                        } else {
+                            Toast.makeText(requireContext(), "Invalid User", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
