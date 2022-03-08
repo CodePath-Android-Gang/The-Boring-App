ecaa6b923046e3e95f3006814b583d8a5f596602
Volan360 <avalonsallam@gmail.com>
2022-03-08 14:23:29 -0800

    Implemented dropdown menu for types

--- app/src/main/java/com/example/theboringapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/theboringapp/fragments/SearchFragment.kt
@@ -1,18 +1,14 @@
 package com.example.theboringapp.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import com.example.theboringapp.BoredActivity
-import com.example.theboringapp.MainActivity
+import android.widget.*
 import com.example.theboringapp.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.theboringapp.SpinnerActivity
 
 class SearchFragment : Fragment() {
 
@@ -26,17 +22,35 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //Dropdown menu code
+        val spinner: Spinner = view.findViewById(R.id.drop_downType)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            view.context,
+            R.array.types_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = SpinnerActivity()
         //Set onClickListener for search submit and random buttons
         view.findViewById<Button>(R.id.submitBtn).setOnClickListener{
             val participants = view.findViewById<EditText>(R.id.etParticipant).text.toString()
             val price = view.findViewById<EditText>(R.id.etPrice).text.toString()
-            val type = view.findViewById<EditText>(R.id.etType).text.toString()
+            val type = spinner.selectedItem.toString()
             val accessibility = view.findViewById<EditText>(R.id.etAccessibility).text.toString()
+
+
 
             var hasFilter: Boolean = false
             var filter: String = ""
 
-            if(type != "") {
+            if(type != "Any" && type != "") {
                 hasFilter = true
                 filter = "?"
                 filter += "type=$type"
